143fa5a0bf3d0f9cf84aa62e3444c9c07ac68517
dean parkin <deanparkin@live.co.uk>
2020-06-26 15:48:34 +0100

    PlanD Ver 1.0.0

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 // Plan;D Main js document
+
+const parallax = document.getElementById('paralax');
+
+window.addEventListener("scroll", function () {
+  var offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 1.5 + "px";
+});
